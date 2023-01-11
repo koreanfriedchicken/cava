7676686cdd4ad9924296dd2cbafc091efc21bac8
vvllnn <90084441+vvllnn@users.noreply.github.com>
2023-01-11 10:49:45 -0600

    working on menu skeleton

---
+++ src/components/Hero.js
@@ -0,0 +1,18 @@
+const Hero = () => {
+  return (
+    <div className='hero'>
+      <div className='hero__content'>
+        <img src='heroimage.avif'/>
+        <div className='hero__right'>
+          <h1>ROASTED WHITE SWEET POTATOES</h1>
+          <p>Back by popular demand - generous hunks of creamy white sweet potatoes, roasted with bold and savory
+            Mediterranean spices
+          </p>
+          <div>Build your custom bowl</div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Hero
---
+++ src/components/Navbar.js
@@ -0,0 +1,29 @@
+const Navbar = () => {
+  return (
+    <div className='navbar'>
+      <div className='navbar__logo'>
+        <img src='cavalogo.svg' />
+      </div>
+      <div className='navbar__links'>
+        <div>Menu</div>
+        <div>Gifting</div>
+        <div>Rewards</div>
+        <div>Catering</div>
+        <div>Locations</div>
+      </div>
+      <div className='navbar__right'>
+        <div className='navbar__signin'>
+          <img src='user.svg'></img>
+          <div>Sign In</div>
+        </div>
+        <div className='navbar__bag'>
+          <img src='bag.svg'></img>
+        </div>
+        <div className='navbar__order'>Order</div>
+      </div>
+
+    </div>
+  )
+}
+
+export default Navbar
