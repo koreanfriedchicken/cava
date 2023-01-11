471bed6943741b3dd98cbec7ef90b0d7baa30116
vvllnn <90084441+vvllnn@users.noreply.github.com>
2023-01-11 17:29:55 -0600

    hero section done

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -8,7 +8,11 @@ const Hero = () => {
           <p>Back by popular demand - generous hunks of creamy white sweet potatoes, roasted with bold and savory
             Mediterranean spices
           </p>
-          <div>Build your custom bowl</div>
+          <div className='hero__build'>Build your custom bowl</div>
+          <div className='hero__arrows'>
+            <img src='leftarrow.svg'/>
+            <img src='rightarrow.svg'/>
+          </div>
         </div>
       </div>
     </div>
