1397e25cd1dec194d7d1aed553c4407569486e2b
vvllnn <90084441+vvllnn@users.noreply.github.com>
2023-01-11 18:11:15 -0600

    starting footer

---
+++ src/components/Menu.js
@@ -0,0 +1,47 @@
+const Menu = () => {
+  return (
+    <div className='menu'>
+      <div className='menu__options'>
+        <div>Bowls</div>
+        <div>Pitas</div>
+        <div>Kid's Meal</div>
+        <div>Drinks</div>
+        <div>Sides</div>
+        <div>Desserts</div>
+      </div>
+      <h1>BOWLS</h1>
+      <div className='menu__grid'>
+          <div className='menu__item'>
+            <img src='balsamicbowl.avif'/>
+            <h2>BALSAMIC DATE CHICKEN</h2>
+            <h3>645 Cal</h3>
+            <p>Grilled chicken, eggplant dip, red pepper hummus, corn, brown rice, spinach,
+              balsamic date vinaigrette, more.</p>
+          </div>
+          <div className='menu__item'>
+            <img src='balsamicbowl.avif'/>
+            <h2>BALSAMIC DATE CHICKEN</h2>
+            <h3>645 Cal</h3>
+            <p>Grilled chicken, eggplant dip, red pepper hummus, corn, brown rice, spinach,
+              balsamic date vinaigrette, more.</p>
+          </div>
+          <div className='menu__item'>
+            <img src='balsamicbowl.avif'/>
+            <h2>BALSAMIC DATE CHICKEN</h2>
+            <h3>645 Cal</h3>
+            <p>Grilled chicken, eggplant dip, red pepper hummus, corn, brown rice, spinach,
+              balsamic date vinaigrette, more.</p>
+          </div>
+          <div className='menu__item'>
+            <img src='balsamicbowl.avif'/>
+            <h2>BALSAMIC DATE CHICKEN</h2>
+            <h3>645 Cal</h3>
+            <p>Grilled chicken, eggplant dip, red pepper hummus, corn, brown rice, spinach,
+              balsamic date vinaigrette, more.</p>
+          </div>
+      </div>
+    </div>
+  )
+}
+
+export default Menu
